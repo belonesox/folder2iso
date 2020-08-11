37b92401d361591180ee647d0bbeb843ddcb2fcc
Stas Fomin <stas-fomin@yandex.ru>
2020-08-11 16:54:51 +0300

    fix reqs

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['pycdlib']
 
 setup_requirements = [ 
-'pycdlib',
+
 ]
 
 test_requirements = [ ]
